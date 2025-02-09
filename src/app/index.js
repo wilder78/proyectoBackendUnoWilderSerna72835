45cc89f250b33587c2e189c266df4dda7fd5fa81
wilder78 <173951485+wilder78@users.noreply.github.com>
2025-02-09 18:52:58 -0500

    Codificación de llamado por web.

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import path from "path";
 import { config } from "../config/index.js";
-import { ProductRouter, CartsRouter } from "../routes/index.js";
+import { ProductRouter, CartsRouter, ViewsRouter } from "../routes/index.js";
 import { validateInputHome } from "../middleware/validationMiddleware.js";
+import { engine } from "express-handlebars";
 
 
 const initApp = () => {
@@ -14,13 +15,20 @@ const initApp = () => {
 
   // Route to display view by default. --//-- Ruta para mostrar vista por por defecto.
   app.use(express.static(path.join(config.dirname, "public")));
+  app.set("views", config.dirname + "src/views");
 
   // Authentication with middleware. --//-- Autenticación con el software intermedio.
   app.use(validateInputHome);
 
+  app.engine("handlebars", engine());
+  app.set("view engine", "handlebars");
+
+  // Rooter para las vistas.
+  app.use('/', ViewsRouter);
+
   // Export links for routes. --//-- Enlaces de exportación de las rutas.
-  app.use('/api/products', ProductRouter);
-  app.use('/api/carts', CartsRouter)
+  app.use("/api/products", ProductRouter);
+  app.use("/api/carts", CartsRouter);
 
   return app;
 };
