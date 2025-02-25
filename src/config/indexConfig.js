94f31d9da770bf7131e3510862c982c84ab5e32b
wilder78 <173951485+wilder78@users.noreply.github.com>
2025-02-25 10:20:29 -0500

    Modificación a la conexión de la base de datos y el servidor.

--- src/config/indexConfig.js
+++ src/config/indexConfig.js
@@ -6,13 +6,14 @@ import { dirname, join } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 export const __dirname = join(dirname(__filename), '../../')
 
+const USERMONGODB = process.env.USERMONGODB
+const PASSWORDMONGODB = process.env.PASSWORDMONGODB
+
 export const config = {
   dirname: __dirname,
   PORT: 8080,
   db: {
     // coderhouse sera la base de datos a la cual se conectará.
-    connectionString: `mongodb+srv://company:M7D5W8tE8mIR72la@cluster0.gw9yn.mongodb.net/coderhouse?retryWrites=true&w=majority&appName=Cluster0`,
+    connectionString: `mongodb+srv://${USERMONGODB}:${PASSWORDMONGODB}@cluster0.gw9yn.mongodb.net/coderhouse?retryWrites=true&w=majority&appName=Cluster0`,
   },
 }
-
-console.log(config.dirname)
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-import initApp from './app/index.js'
-import { config } from './config/indexConfig.js'
-import { initMongoDBAtlas } from './db/index.js'
-
-const app = initApp()
+import initApp from "./app/index.js";
+import { config } from "./config/indexConfig.js";
+import { initMongoDBAtlas } from "./db/index.js";
 
 //Connection MongoDB Atlas
-initMongoDBAtlas()
+await initMongoDBAtlas();
+
+const app = initApp();
 
 const server = app.listen(config.PORT, () => {
-  console.info(`Server listen on: http://localhost:${config.PORT}`)
-})
+  console.info(`Server listen on: http://localhost:${config.PORT}`);
+});
